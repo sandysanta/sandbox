212f4d63de7ab33d504fb44b969938a48b0ed2cb
sandy <sandysanta@demo.com>
2018-11-18 19:45:54 +0100

    dockerize task is working and container can be run with application/service on a default CMD entry of the container

--- helloDocker/src/main/java/com/demo/HelloDocker.java
+++ helloDocker/src/main/java/com/demo/HelloDocker.java
@@ -2,9 +2,13 @@ package com.demo;
 
 public class HelloDocker {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
 
-        System.out.println("Hello Docker ...");
+        while(true) {
+            Thread.sleep(1000);
+
+            System.out.println("Hello Docker ...");
+        }
 
     }
 
